0c4703d92ec2f6a595d84337023bb8e8b5a3ce9f
ymmmtym <h.yukihisa.378@gmail.com>
2019-08-04 21:18:06 +0900

    modified port 5000

--- app/run.py
+++ app/run.py
@@ -42,4 +42,4 @@ def result():
         return render_template("index.html", search_dic = search_dic)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',port=80,threaded=True)
+  app.run(host='0.0.0.0',port=5000,threaded=True)
