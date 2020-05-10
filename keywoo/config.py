77da2cb15a9a973ef0a5601e33315926d9704b80
ymmmtym <h.yukihisa.378@gmail.com>
2020-05-11 02:00:52 +0900

    modify localenv to productionenv

--- keywoo/config.py
+++ keywoo/config.py
@@ -1,10 +1,8 @@
 import os
 
 SQLALCHEMY_DATABASE_URI = "postgresql://keywoo:keywoo@db:5432/keywoo"
-# SQLALCHEMY_DATABASE_URI = "sqlite:///keywoo.db" # local
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG=True
 THREADED = True
-TESTING=True
 JSON_AS_ASCII = False
 SECRET_KEY = os.urandom(12)
