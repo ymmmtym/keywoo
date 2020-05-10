f8e90bf93abf29499777fff518aa2dc6426df09e
ymmmtym <h.yukihisa.378@gmail.com>
2020-05-10 11:51:22 +0900

    add User class

--- keywoo/views.py
+++ keywoo/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, request, jsonify, flash
+from flask import render_template, url_for, request, jsonify, flash
 from keywoo import app
 from keywoo import db
 from keywoo.models.sites import Site
+from keywoo.models.users import User
 import json
 
 
@@ -41,3 +42,12 @@ def result():
     else:
         flash('failed')
         return render_template("index.html", search_dic = search_dic)
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.get(user_id)
+
+# @app.route("/secret")
+# @login_required
+# def secret():
+#     return "secret contents"
