71924e688bed2b8cb3b6549b46f1cdc3b88bed77
ymmmtym <h.yukihisa.378@gmail.com>
2020-05-10 11:49:00 +0900

    add login manager instance

--- keywoo/__init__.py
+++ keywoo/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 def get_toppage(str):
@@ -11,5 +12,9 @@ app.config.from_object('keywoo.config')
 app.jinja_env.globals['get_toppage'] = get_toppage
 
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
 import keywoo.views
